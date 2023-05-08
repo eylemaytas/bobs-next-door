b5f1c79698de9623dc5cfb4eb49de86a34aea53a
Eylem Aytas <eylem.aytas@hotmail.com>
2023-05-08 12:13:31 -0400

    Modifies components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,69 @@ import Search from './components/Search'
 import NewStoreForm from './components/NewStoreForm';
 import StoreList from './components/StoreList';
 
+import { useState, useEffect } from "react"
+
 function App() {
+
+  const [stores, setStores] = useState([])
+  const[formData, setFormData] = useState({
+    name: "",
+    image: "",
+    season: 0,
+    episode: 0
+  })
+
+  const [searchText, setSearchText] = useState("")
+
+
+
+ useEffect(() => {
+  fetch("http://localhost:8085/stores")
+  .then(res => res.json())
+  .then(storeData => setStores(storeData))
+ }, [])
+
+ function addNewStore(event){
+  event.preventDefault()
+  fetch("http://localhost:8085/stores", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body:JSON.stringify(formData)
+  })
+  .then(res => res.json())
+  .then(newStore => setStores([...stores, newStore]))
+ }
+
+ function updateFormData(event){
+  if(event.target.id === "season" || event.target.id === "episode"){
+    setFormData({...formData, [event.target.id]: Number(event.target.value)})
+  }
+  else{
+setFormData({...formData, [event.target.id]: event.target.value})
+ }
+}
+
+function updateSearchText(event){
+setSearchText(event.target.value)
+}
+
+const filteredStores = stores. filter(store => {
+if (searchText === "") {
+  return true
+}
+return store.name.toLowerCase().includes(searchText.toLowerCase())
+})
+
   return (
     <div className="main-container">
       <img src="/images/bobsburgers.png" />
       <h1>Neighbor Stores</h1>
-      <Search />
-      <NewStoreForm />
-      <StoreList />
+      <Search updateSearchText={updateSearchText}/>
+      <NewStoreForm addNewStore={addNewStore} updateFormData={updateFormData}/>
+      <StoreList stores = {filteredStores} />
     </div>
   );
 }
--- src/components/NewStoreForm.js
+++ src/components/NewStoreForm.js
@@ -1,13 +1,13 @@
 import React from "react"
 
-function NewStoreForm() {
+function NewStoreForm({addNewStore, updateFormData}) {
 
     return(
-        <form>
-            <input type="text" id="name" placeholder="Store Name"/>
-            <input type="text" id="image" placeholder="Image URL" />
-            <input type="number" id="season" placeholder="Season" step="1"/>
-            <input type="number" id="episode" placeholder="Episode" step="1"/>
+        <form onSubmit={addNewStore}>
+            <input onChange={updateFormData} type="text" id="name" placeholder="Store Name"/>
+            <input onChange={updateFormData} type="text" id="image" placeholder="Image URL" />
+            <input onChange={updateFormData} type="number" id="season" placeholder="Season" step="1"/>
+            <input onChange={updateFormData} type="number" id="episode" placeholder="Episode" step="1"/>
             <button type="submit">Add Store</button>
         </form>
     )
